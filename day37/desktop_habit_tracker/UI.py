0c6a56631618dd9ca093e505773ae4a4e7a1a477
Luciano MELHEM <lucianomelhem@hotmail.com>
2023-04-11 16:17:05 -0300

    Progress on desktop habit tracker day 38 now

--- day37/desktop_habit_tracker/UI.py
+++ day37/desktop_habit_tracker/UI.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+
+import requests.exceptions
 from cairosvg import svg2png
 from habit_tracker import HabitTracker
 
@@ -14,7 +16,7 @@ class UI:
         self.window = Tk()
         self.window.config(pady=20, padx=20)
         title = Label(text="Tap on the buttons to create your image")
-        title.grid(row=0, column=0)
+        title.grid(row=7, column=0, columnspan=4)
         self.window.title("Pixela image generator")
 
         if user:
@@ -25,9 +27,9 @@ class UI:
         image_button = Button(text="Create image", padx=2, pady=2, font=("Ariel", 20, "italic"), bg="white",
                               command=self.show_image)
         # Buttons
-        image_button.grid(row=1, column=1)
+        image_button.grid(row=5, column=1)
         add_pixel_button = Button(text="Add pixel", padx=2, pady=2, font=("Ariel", 20, "italic"), bg="white",
-                                  command=self.add_pixel)
+                                  command=self.add_pixel_today)
         add_pixel_button.grid(row=2, column=1)
 
         add_pixel_button = Button(text="Modify pixel", padx=2, pady=2, font=("Ariel", 20, "italic"), bg="white",
@@ -38,24 +40,50 @@ class UI:
                                      command=self.delete_pixel)
         delete_pixel_button.grid(row=3, column=1)
 
+        create_graph = Button(text="Create graph", padx=2, pady=2, font=("Ariel", 20, "italic"), bg="white",
+                                     command=self.create_graph)
+        create_graph.grid(row=1, column=1)
+
+        self.graph_type_input = StringVar(self.window)
+
+        self.graph_type_button_int = Radiobutton(text="int", variable=self.graph_type_input, value=True, width=10,
+                                             command=self.radio_button_select_int)
+        self.graph_type_button_float = Radiobutton(text="float", variable=self.graph_type_input, value=False, width=10,
+                                             command=self.radio_button_select_float)
+        self.graph_type_button_int.grid(row=4, column=3)
+        self.graph_type_button_float.grid(row=4, column=4)
+
         # Labels
-        date_label = Label(text="Date (Example 2023-01-01): ")
+        date_label = Label(text="Date (Example 2023-01-20): ")
         date_label.grid(row=1, column=2)
         self.date = Entry(width=30)
-        self.date.grid(row=1, column=3)
+        self.date.grid(row=1, column=3, columnspan=2)
 
         pixel_size = Label(text="Pixel Size (a number): ")
         pixel_size.grid(row=2, column=2)
         self.pixel_size = Entry(width=30)
-        self.pixel_size.grid(row=2, column=3)
+        self.pixel_size.grid(row=2, column=3, columnspan=2)
+
+        graph_name_label = Label(text="Graph name: ")
+        graph_name_label.grid(row=3, column=2)
+        self.graph_name_input = Entry(width=30)
+        self.graph_name_input.grid(row=3, column=3, columnspan=2)
+
+        graph_type_label = Label(text="Graph type: ")
+        graph_type_label.grid(row=4, column=2)
+
+        graph_units_label = Label(text="Graph units: ")
+        graph_units_label.grid(row=5, column=2)
+        self.graph_units_input = Entry(width=30)
+        self.graph_units_input.grid(row=5, column=3, columnspan=2)
 
         # Graph List
-        graphs_list = Listbox(width=20, height=3, selectborderwidth=5, listvariable=self.habit_tracker.graphs)
+        self.graphs_list = Listbox(width=20, height=3, selectborderwidth=5, listvariable=self.habit_tracker.graphs)
         for graph in self.habit_tracker.graphs:
-            graphs_list.insert(0, graph["name"])
-        graphs_list.grid(row=3, column=3)
+            self.graphs_list.insert(END, graph["name"])
+        self.graphs_list.grid(row=6, column=3, columnspan=2)
 
-        graphs_list.bind("<<ListboxSelect>>", self.graphs_callback)
+        self.graphs_list.bind("<<ListboxSelect>>", self.graphs_callback)
 
         self.current_graph_id = None
 
@@ -73,7 +101,7 @@ class UI:
         image = PhotoImage(file="./output.png")
         self.canvas.config(width=image.width(), height=image.height())
         self.canvas.create_image(0, 0, image=image, anchor="nw")
-        self.canvas.grid(row=1, column=0, rowspan=4, padx=20, pady=20)
+        self.canvas.grid(row=8, column=1, columnspan=4, padx=20, pady=20)
         self.window.mainloop()
 
     def login_open_main_window(self):
@@ -97,14 +125,87 @@ class UI:
         self.log_in_entry.grid(row=1, column=1)
         self.window2.mainloop()
 
-    def add_pixel(self):
-        self.habit_tracker.add_pixel_today()
+    def create_graph(self):
+        g_name = self.graph_name_input.get().strip()
+        g_type = str(self.graph_type_input).strip()
+        g_units = self.graph_units_input.get().strip()
+        # Validation
+        if g_type == "PY_VAR0":
+            messagebox.showinfo("Error", message="Missing selection of graph type")
+            return
+        if not g_name:
+            messagebox.showinfo("Error", message="Missing graph name")
+            return
+        if not g_units:
+            messagebox.showinfo("Error", message="Missing graph units")
+            return
+
+        #Api call
+        try:
+            print(f"Graph name: {g_name}. Graph type: {g_type}. Graph units: {g_units}")
+            self.habit_tracker.create_graph(graph_name=g_name, graph_type=g_type, graph_units=g_type)
+            self.clear_inputs()
+            #Reset graphs in list
+            self.graphs_list.config(listvariable=self.habit_tracker.graphs)
+            messagebox.showinfo(message=f"Graph created. Graph name: {g_name}. Graph type: {g_type}. Graph units: {g_units}")
+        except requests.exceptions.HTTPError as e:
+            print(e)
+
+    def add_pixel_today(self):
+        g_id = self.current_graph_id
+        print(g_id)
+        pixel_size = self.pixel_size.get().strip()
+        # Validation
+        if not g_id:
+            messagebox.showinfo("Error",message="Missing graph selection")
+            return
+        if not pixel_size:
+            messagebox.showinfo("Error",message="Missing quantity")
+            return
+        print(self.habit_tracker.add_pixel_today(graph_id=g_id, qty=pixel_size))
+        self.clear_inputs()
 
     def delete_pixel(self):
-        self.habit_tracker.delete_pixel()
+        g_id = self.current_graph_id
+        date = "".join(str(self.date).strip().split("-"))
+        # Validation
+        if not g_id:
+            messagebox.showinfo("Error", message="Missing graph selection")
+            return
+        if not date:
+            messagebox.showinfo("Error", message="Missing date")
+            return
+        print(self.habit_tracker.delete_pixel(graph_id=g_id, date=date))
+        self.clear_inputs()
 
     def modify_pixel(self):
-        self.habit_tracker.modify_pixel()
+        g_id = self.current_graph_id
+        date = "".join(str(self.date).strip().split("-"))
+        pixel_size = self.pixel_size.get().strip()
+        # Validation
+        if not g_id:
+            messagebox.showinfo("Error", message="Missing graph selection")
+            return
+        if not date:
+            messagebox.showinfo("Error", message="Missing date")
+            return
+        if not pixel_size:
+            messagebox.showinfo("Error", message="Missing quantity")
+            return
+        self.habit_tracker.modify_pixel(date=date, graph_id=g_id, pixel_qty=pixel_size)
+        self.clear_inputs()
+
+    def radio_button_select_int(self):
+        self.graph_type_input = "int"
+
+    def radio_button_select_float(self):
+        self.graph_type_input = "float"
+
+    def clear_inputs(self):
+        self.graph_name_input.delete(0, END)
+        self.graph_units_input.delete(0, END)
+        self.date.delete(0, END)
+        self.pixel_size.delete(0, END)
 
 
 ui = UI()
